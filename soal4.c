bc9275b0fed0c160ead2a0670a02af6f5c33f05f
zaayn <muzayyin937@gmail.com>
2017-10-25 10:39:15 +0700

    perbaikan

--- soal4.c
+++ soal4.c
@@ -3,13 +3,34 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
-int main(int argv,int *argc[]){
-	int i;	
+void * faktorial(void * arg)
+{
+	int i;
+	int num = (int) arg ;
+	long long jumlah = 1 ;
+	for(i=1;i<= num;i++) 
+	{
+		jumlah *=(long long) i;
+	}
+	printf("Hasil %d! = %lld\n",num,jumlah); 
+}
+
+int main(int argv,char *argc[])
+{
+	int i,l;	
 	pthread_t t1[argv];
-	for( i=1; i<=argv;i++){
+	for( i=1; i<argv;i++)
+	{
+		
 		int num = atoi(argc[i]);
-		pthread_create(&(t1[i]),NULL,faktorial,&num);
+		pthread_create(&t1[i],NULL,faktorial,(void *)num);
+		
+	}
+	for(l=1;l<argv;l++)
+	{
+		pthread_join(t1[l],NULL);
 	}
       return 0;
 }

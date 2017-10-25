73ae665bb0377ec2c6acda69cb6c4793fe3d59d6
otnayrus <vinsensiusindra@gmail.com>
2017-10-25 09:05:55 +0700

    Perbaikan passing arg + main

--- soal5.c
+++ soal5.c
@@ -4,15 +4,14 @@
 #include <string.h>
 #include <unistd.h>
 
-//struct passer{
-//  char arr[100]; 
-//};
+struct passer{
+  char arr[100]; 
+};
 
 void* carikata (void * arg){
-	char kata[100];
-	string dicari = (string) arg;
-//	struct passer * cari = (struct passer *) arg;
-//	strcpy(dicari,arg->arr);
+	char kata[100],dicari[100];
+	struct passer * cari = (struct passer *) arg;
+	strcpy(dicari,cari->arr);
 	int jumlah = 0;	
 	FILE * novel;
 	novel = fopen("novel.txt","r");
@@ -26,12 +25,12 @@ fclose(novel);
 int main(int argv, char * argc[]){
 	pthread_t t1[argv];
 	int i;
-//	struct passer listkata;
-	for(i=1;i<=argv;i++){
-	//strcpy(listkata.arr,argc[i]);		
-	pthread_create(&(t1[i]), NULL , carikata ,(void *) &argc[i]);
-	pthread_join(t1[i],NULL);	
+	struct passer listkata;
+	for(i=1;i<argv;i++){
+	strcpy(listkata.arr,argc[i]);		
+	pthread_create(&(t1[i]), NULL , carikata ,(void *) &listkata);
 }
+for(i=1;i<argv;i++) pthread_join(t1[i],NULL);
 	
-return 0;
+ return 0;
 }

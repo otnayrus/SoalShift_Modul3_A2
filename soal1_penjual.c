74e9c9188944e47d2a6a98951637d414b39c86d7
otnayrus <vinsensiusindra@gmail.com>
2017-10-25 14:07:52 +0700

    Add comparator function dan isi main penjual

--- soal1_penjual.c
+++ soal1_penjual.c
@@ -2,22 +2,45 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
+#include <string.h>
+
+int guncomparator(char * arr){
+	if(strcmp(arr,"MP4A1")==0) return 0;
+	else if(strcmp(arr,"PM2-V1")==0) return 1;
+	else if(strcmp(arr,"SPR-3")==0) return 2;
+	else if(strcmp(arr,"SS2-V5")==0) return 3;
+	else if(strcmp(arr,"SPG1-V3")==0) return 4;
+	else if(strcmp(arr,"MINE")==0) return 5;
+	else return -1;
+}
 
 void main()
 {
         key_t key = 1234;
-        int *MP4A1, *PM2-V1, *SPR-3, *SS2-V5, *SPG1-V3, *MINE;
-	int i;
-        for(i=0;i<6;i++) shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-        value = shmat(shmid, NULL, 0);
+	char plus[8];
+	int i , n , shmid,val, *gun;
+        shmid = shmget(key, 6*sizeof(int), IPC_CREAT | 0666);
+        gun = (int*)shmat(shmid, NULL, 0);
+//inisialisasi
+	for(i=0;i<6;i++) gun[i]=0;
 
-        *value = 10;
+	while(1){
+	  printf("1. Lihat stok , 2. Tambah Senjata : ");
+	  scanf("%d",&n);
+	  if(n==1){
+	    for(i=0;i<6;i++) if(gun[i]!=0) printf("%d=%d\n",i,gun[i]);
+	  }
+	  else if(n==2){
+	    printf("Tambah : ");
+	    scanf("%s%d",plus,&val);
+	    i=guncomparator(plus);
+	    if(i!=-1) gun[i]+=val;
+	    else printf("Gak jual itu\n");
+	  }
+	  else if(n==0) break;
+	  else printf("Input salah\n");
+	}
 
-        printf("Program 1 : %d\n", *value);
-
-        sleep(5);
-
-        printf("Program 1: %d\n", *value);
-        shmdt(value);
+        shmdt(gun);
         shmctl(shmid, IPC_RMID, NULL);
 }

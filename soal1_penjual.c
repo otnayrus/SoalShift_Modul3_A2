9576d13ac85279c25ae7a6be8222d2120c7154c5
zaayn <muzayyin937@gmail.com>
2017-10-25 12:16:06 +0700

    Initial

---
+++ soal1_penjual.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+void main()
+{
+        key_t key = 1234;
+        int *MP4A1, *PM2-V1, *SPR-3, *SS2-V5, *SPG1-V3, *MINE;
+	int i;
+        for(i=0;i<6;i++) shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+        value = shmat(shmid, NULL, 0);
+
+        *value = 10;
+
+        printf("Program 1 : %d\n", *value);
+
+        sleep(5);
+
+        printf("Program 1: %d\n", *value);
+        shmdt(value);
+        shmctl(shmid, IPC_RMID, NULL);
+}
